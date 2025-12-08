c93c3c9916dc717b9d61639ea8bd6f95e1f9ddc3
Khoi <khoidang01234@gmail.com>
2025-12-08 18:19:02 +0700

    feat: add style ProductHero

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import data from "./data.json";
+import ProductHero from "./components/ProductHero";
+import ProductCart from "./components/ProductCart";
+
 const App = () => {
-  return <div>App</div>;
+  const [cartItems, setCartItems] = useState([]);
+  const [products] = useState(data);
+
+  const handleAddToCart = () => {};
+
+  const handleRemoveFromCart = () => {};
+
+  return (
+    <main className="min-h-screen relative">
+      <div className="grid grid-cols-[2fr_1fr] p-10">
+        <ProductHero products={products} onAddToCart={handleAddToCart} />
+        <ProductCart onRemoveFromCart={handleRemoveFromCart} />
+      </div>
+    </main>
+  );
 };
 
 export default App;
---
+++ src/components/ProductHero.jsx
@@ -0,0 +1,31 @@
+const ProductHero = ({ products }) => {
+  return (
+    <section>
+      <h1 className="text-3xl font-bold text-Rose-900">Desserts</h1>
+
+      <div className="grid md:grid-cols-3 grid-cols-2 gap-4">
+        {products.map((product) => (
+          <div key={product.category} className="relative">
+            <img
+              className="object-cover rounded-lg"
+              src={product.image.desktop.replace("./assets/images/", "")}
+              alt=""
+            />
+            <button className="flex gap-2 items-center w-full max-w-[140px] border-Rose-900 border absolute bg-Rose-50 rounded-3xl px-3 py-2 left-1/2 -translate-x-1/2 bottom-1/4">
+              <img src="icon-add-to-cart.svg" alt="" />
+              <p>Add to Cart</p>
+            </button>
+
+            <p className="text-Rose-500 pt-6">{product.category}</p>
+            <p>{product.name}</p>
+            <p className="text-Red font-semibold">
+              ${product.price.toFixed(2)}
+            </p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default ProductHero;
